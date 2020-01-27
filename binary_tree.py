19021f536bbec4afa75046eea690757511a4cd38
Hairizuan <hairizuanbinnoorazman@gmail.com>
2020-01-28 01:36:02 +0800

    feat: Initial set of algo exercises

---
+++ binary_tree.py
@@ -0,0 +1,62 @@
+class Node:
+    def __init__(self, key, left=None, right=None):
+        self.value = key
+        self.left = left
+        self.right = right
+
+
+def inorder_traversal(nodes):
+    item = []
+    if nodes.left is not None:
+        item = item + inorder_traversal(nodes.left)
+    item = item + [nodes.value]
+    if nodes.right is not None:
+        item = item + inorder_traversal(nodes.right)
+    return item
+
+
+def preorder_traversal(nodes):
+    item = []
+    item = item + [nodes.value]
+    if nodes.left is not None:
+        item = item + preorder_traversal(nodes.left)
+    if nodes.right is not None:
+        item = item + preorder_traversal(nodes.right)
+    return item
+
+
+def postorder_traversal(nodes):
+    item = []
+    if nodes.left is not None:
+        item = item + postorder_traversal(nodes.left)
+    if nodes.right is not None:
+        item = item + postorder_traversal(nodes.right)
+    item = item + [nodes.value]
+    return item
+
+
+def levelorder_traversal(tree, level):
+    current_level = 0
+    item = []
+    if current_level == level:
+        item = item + [tree.value]
+    if current_level < level:
+        if tree.left is not None:
+            item = item + levelorder_traversal(tree.left, level-1)
+        if tree.right is not None:
+            item = item + levelorder_traversal(tree.right, level-1)
+    return item
+
+
+def insert_binary_search_tree(tree, item):
+    if item >= tree.value:
+        if tree.right is None:
+            tree.right = Node(item)
+        else:
+            insert_binary_search_tree(tree.right, item)
+    else:
+        if tree.left is None:
+            tree.left = Node(item)
+        else:
+            insert_binary_search_tree(tree.left, item)
+    return tree
---
+++ test_binary_tree.py
@@ -0,0 +1,98 @@
+import pytest
+
+from binary_tree import *
+
+
+@pytest.mark.parametrize(
+    "tree,expected",
+    [
+        pytest.param(
+            Node(4), [4]
+        ),
+        pytest.param(
+            Node(1, Node(2, Node(4), Node(5)), Node(3)), [4, 2, 5, 1, 3]
+        ),
+        pytest.param(
+            Node(1, Node(2, Node(4), Node(5)), Node(
+                3, Node(8, Node(12)), Node(11))), [4, 2, 5, 1, 12, 8, 3, 11]
+        ),
+    ],
+)
+def test_inorder_traversal(tree, expected):
+    answer = inorder_traversal(tree)
+    assert answer == expected
+
+
+@pytest.mark.parametrize(
+    "tree,expected",
+    [
+        pytest.param(
+            Node(4), [4]
+        ),
+        pytest.param(
+            Node(1, Node(2, Node(4), Node(5)), Node(3)), [1, 2, 4, 5, 3]
+        ),
+        pytest.param(
+            Node(1, Node(2, Node(4), Node(5)), Node(
+                3, Node(8, Node(12)), Node(11))), [1, 2, 4, 5, 3, 8, 12, 11]
+        ),
+    ],
+)
+def test_preorder_traversal(tree, expected):
+    answer = preorder_traversal(tree)
+    assert answer == expected
+
+
+@pytest.mark.parametrize(
+    "tree,expected",
+    [
+        pytest.param(
+            Node(4), [4]
+        ),
+        pytest.param(
+            Node(1, Node(2, Node(4), Node(5)), Node(3)), [4, 5, 2, 3, 1]
+        ),
+    ],
+)
+def test_postorder_traversal(tree, expected):
+    answer = postorder_traversal(tree)
+    assert answer == expected
+
+
+@pytest.mark.parametrize(
+    "tree,level,expected",
+    [
+        pytest.param(
+            Node(4), 0, [4]
+        ),
+        pytest.param(
+            Node(1, Node(2, Node(4), Node(5)), Node(3)), 1, [2, 3]
+        ),
+        pytest.param(
+            Node(1, Node(2, Node(4), Node(5)), Node(3)), 2, [4, 5]
+        ),
+        pytest.param(
+            Node(1, Node(2, Node(4), Node(5)), Node(3)), 0, [1]
+        ),
+    ],
+)
+def test_levelorder_traversal(tree, level, expected):
+    answer = levelorder_traversal(tree, level)
+    assert answer == expected
+
+
+@pytest.mark.parametrize(
+    "tree,item,expected",
+    [
+        pytest.param(
+            Node(4), 2, [4, 2]
+        ),
+        pytest.param(
+            Node(1, Node(-1), Node(3)), 4, [1, -1, 3, 4]
+        ),
+    ],
+)
+def test_insert_binary_search_tree(tree, item, expected):
+    answer = insert_binary_search_tree(tree, item)
+    answer_int = preorder_traversal(answer)
+    assert answer_int == expected
